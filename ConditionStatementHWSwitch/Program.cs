a29a886d71c832e8bc6a3d7e44b32f17c7c595e9
mgapp <mgapp@MIKEGYO>
2025-06-03 18:13:36 -0400

    Add project files.

---
+++ ConditionStatementHWSwitch/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConditionStatementHWSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name: ");
+            string? firstName = Console.ReadLine();
+            bool isValid = int.TryParse(firstName, out int numberName);
+            while (firstName == "" || isValid == true)
+            {
+                Console.WriteLine("Please enter your actual name.");
+                Console.Write("What is your name: ");
+                firstName = Console.ReadLine();
+                isValid = int.TryParse(firstName, out numberName);
+                switch (isValid)
+                {
+                    case false:
+                        break;
+                    default:
+                        continue;
+                }
+                break;
+            }
+            switch (firstName.ToLower())
+            {
+                case "tim" or "timothy":
+                    Console.WriteLine("Hello Professor");
+                    break;
+                default:
+                    Console.WriteLine("Hello Student");
+                    break;
+            }
+        }
+    }
+}
